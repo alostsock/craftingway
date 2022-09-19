c485bdb5fb1d39798d6abe4503968c041223b522
alostsock <hi@alostsock.dev>
2022-09-19 06:45:42 -0400

    add submodule, test wasm loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+import init, { recipes } from 'crafty'
+// import wasmUrl from 'crafty/crafty_bg.wasm?url';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    init().then(() => {
+      let recipeOptions = recipes(90);
+      console.log(recipeOptions);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      aaa
     </div>
   )
 }

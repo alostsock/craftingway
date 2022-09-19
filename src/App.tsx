b129e0d0e92df6b464a876d9ac0b252d0e039771
alostsock <hi@alostsock.dev>
2022-09-19 15:30:14 -0400

    add pre-build steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 import init, { recipes } from 'crafty'
-// import wasmUrl from 'crafty/crafty_bg.wasm?url';
 
 function App() {
-
   useEffect(() => {
     init().then(() => {
       let recipeOptions = recipes(90);

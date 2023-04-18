b4ced44af12e884a76673f1d5e4f7a4777049302
raylu <90059+raylu@users.noreply.github.com>
2023-04-18 03:47:26 +0000

    set up eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    "settings": {
+        "react": {"version": "detect"},
+    },
+    "ignorePatterns": ["crafty/"],
+    "overrides": [],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module,"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+    ],
+    "rules": {
+        "react/no-unescaped-entities": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unused-vars": ["error", {"args": "none", "destructuredArrayIgnorePattern": "^_"}],
+    },
+}

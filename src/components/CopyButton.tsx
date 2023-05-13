5fa85a7e2d742899c81358cb6633510b1a1915eb
alostsock <hi@alostsock.dev>
2023-05-13 16:00:28 -0400

    tweak CopyButton

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+import "./CopyButton.scss";
+import React, { useEffect, useState } from "react";
 import clsx from "clsx";
-import { observer } from "mobx-react-lite";
+import Emoji from "./Emoji";
+import * as Tooltip from "@radix-ui/react-tooltip";
 
 interface Props extends React.ComponentPropsWithoutRef<"button"> {
   copyText: string;
 }
 
-const CopyButton = observer(function CopyButton({
-  copyText,
-  className,
-  children,
-  ...props
-}: Props) {
+export default function CopyButton({ copyText, className, children, ...props }: Props) {
+  const [isCopied, setIsCopied] = useState(false);
+
   const handleCopy = () => {
     navigator.clipboard.writeText(copyText);
+    setIsCopied(true);
   };
 
-  return (
-    <button className={clsx("CopyButton", className)} onClick={handleCopy} {...props}>
-      {children}
-    </button>
-  );
-});
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsCopied(false), 1000);
+    return () => clearTimeout(timeout);
+  }, [isCopied]);
 
-export default CopyButton;
+  return (
+    <Tooltip.Root open={isCopied ? true : undefined}>
+      <Tooltip.Trigger asChild>
+        <button className={clsx("CopyButton", className)} onClick={handleCopy} {...props}>
+          {children}
+        </button>
+      </Tooltip.Trigger>
+      <Tooltip.Content asChild>
+        <div className="CopyButton content">
+          <Emoji emoji="📋" />
+          <span>{isCopied ? "Copied!" : "Copy"}</span>
+        </div>
+      </Tooltip.Content>
+    </Tooltip.Root>
+  );
+}

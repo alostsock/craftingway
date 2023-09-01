47695e6a011b4382442634d23b6b74702d862c46
alostsock <hi@alostsock.dev>
2023-09-01 02:06:21 -0400

    separate out env in document title

--- src/components/DocumentTitle.tsx
+++ src/components/DocumentTitle.tsx
@@ -6,21 +6,23 @@ export default function DocumentTitle({ prefix }: { prefix?: string | null }) {
 
   const [suffix, setSuffix] = useState("Craftingway");
 
+  const env = window.origin.includes("craftingway-preview")
+    ? " (Preview)"
+    : window.origin.includes("localhost")
+    ? " (Dev)"
+    : "";
+
   useEffect(() => {
     if (location.startsWith("/rotation")) {
       setSuffix("Saved Rotation");
-    } else if (window.origin.includes("craftingway-preview")) {
-      setSuffix("Craftingway (Preview)");
-    } else if (window.origin.includes("localhost")) {
-      setSuffix("Craftingway (Dev)");
     } else {
       setSuffix("Craftingway");
     }
   }, [location]);
 
   useEffect(() => {
-    document.title = prefix ? `${prefix} | ${suffix}` : suffix;
-  }, [prefix, suffix]);
+    document.title = prefix ? `${prefix} | ${suffix}${env}` : `${suffix}${env}`;
+  }, [prefix, suffix, env]);
 
   return null;
 }

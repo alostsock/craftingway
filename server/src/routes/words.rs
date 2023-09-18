6be65fe8bfd34c1bc2d3c61bcece008d860f2ddf
alostsock <hi@alostsock.dev>
2023-09-18 09:07:32 -0400

    debug headers

--- server/src/routes/words.rs
+++ server/src/routes/words.rs
@@ -1,7 +1,12 @@
 use crate::{ApiResult, ApiState};
-use axum::{extract::State, Json};
+use axum::{extract::State, http::HeaderMap, Json};
+use tracing::info;
 
-pub async fn get(State(state): State<ApiState>) -> ApiResult<Json<Vec<String>>> {
+pub async fn get(
+    State(state): State<ApiState>,
+    headers: HeaderMap,
+) -> ApiResult<Json<Vec<String>>> {
+    info!("{:?}", headers);
     let words = (0..50).map(|_| state.slugger.generate(0)).collect();
     Ok(Json(words))
 }

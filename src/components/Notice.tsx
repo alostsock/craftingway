14cbd0b8acdb15e78633a2d7962b88c5cd555da6
alostsock <hi@alostsock.dev>
2026-04-30 16:19:19 -0400

    update for 7.5

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -13,7 +13,7 @@ export default function Notice() {
 
   return (
     <div className="Notice">
-      <Emoji emoji="☄️" /> Updated for patch 7.4!
+      <Emoji emoji="🛣️" /> Updated for patch 7.5!
     </div>
   );
 }
